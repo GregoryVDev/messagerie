157c8b789081aa42334846db6d6553fdcd3892a3
GregoryVDev <arexo@hotmail.fr>
2026-03-03 17:54:38 +0100

    Protected the pseudo variable against malicious scripts and verified the password criteria as well as whether the password complies with the rule

--- src/inscription.php
+++ src/inscription.php
@@ -9,6 +9,36 @@ function validateEmail($email)
 // On vérifie si le formulaire est bien rempli
 if (!empty($_POST)) {
     if (isset($_POST["pseudo"], $_POST["email"], $_POST["pass"], $_POST["pass2"]) && !empty($_POST["pseudo"]) && !empty($_POST["email"]) && !empty($_POST["pass"]) && !empty($_POST["pass2"])) {
+
+        // Il retire les balises dangereuses en HTML, ni mettre des scripts malveillant
+        $pseudo = strip_tags($_POST["pseudo"]);
+
+        if (!validateEmail($_POST["email"])) {
+            $errorMessage = "L'adresse mail est incorrecte.";
+        }
+
+        if (isset($_POST["pass"]) && isset($_POST["pass2"])) {
+            $pass = $_POST["pass"];
+            $pass2 = $_POST["pass2"];
+        }
+
+        // Vérification des critères du mot de passe
+        $passwordPattern = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{6,}$/";
+        // On vérifie si PasswordPattern est bien respecté dans $pass
+        if (!preg_match($passwordPattern, $pass)) {
+            $errorMessage = "Le mot de passe doit contenir :
+            <ul style='color: red;'>
+                <li>- Au moins une lettre majuscule</li>
+                <li>- Au moins une lettre minuscule</li>
+                <li>- Au moins un chiffre</li>
+                <li>- Au moins un caractère spécial</li>
+                <li>- 6 caractères minimum</li>
+            </ul>";
+        } elseif ($pass === $pass2) {
+            $pass = password_hash($_POST["pass"], PASSWORD_ARGON2ID);
+        } else {
+            $errorMessage = "Les mots de pass ne correspondent pas.";
+        }
     }
 }
 
