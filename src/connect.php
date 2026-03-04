f2cbc84f0805a2a836e9bf6daaf00bdfc3e019c1
GregoryVDev <arexo@hotmail.fr>
2026-03-04 16:05:17 +0100

    Connexion in my database

---
+++ src/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+try {
+    $server_name = "db";
+    $db_name = "messagerie";
+    $user_name = "test";
+    $password = "test";
+    $db = new PDO("mysql:host=$server_name; dbname=$db_name;charset=utf8mb4", $user_name, $password);
+} catch (PDOException $e) {
+    echo "Échec de connexion : " . $e->getMessage();
+}
